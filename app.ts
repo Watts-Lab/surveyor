71d9a29bf23708818cfa895509cb3a3d90b54a99
Karan Sampath <ksampath@seas.upenn.edu>
2021-08-18 11:59:50 -0700

    URL arguments that include encrypted `survey_url`, `verification_key` (which forces a verification wrapper on the survey, e.g., `WorkerId`), as well as arbitrary parameters that can be passed in and appear in the data and perhaps some other options that we think are important e.g., expiry. Fixes #5

--- app.ts
+++ app.ts
@@ -5,6 +5,17 @@ import { parseCSV } from "./google_drive";
 import { startServer } from "./server";
 import { readFile } from "fs/promises";
 
+var base_url: string = "https://surveyor.com";
+var survey_url : string = "raw.githubusercontent.com/Watts-Lab/surveyor/main/surveys/CRT.csv";
+
+var key: string[] = [];
+var value: string[] = [];
+let i: number = 0;
+let ext: string = "";
+while(i < key.length){
+  ext = ext + key[i] + "=" + value[i];
+}
+
 async function main(): Promise<void> {
   // should be updated to Load CSV from URL (we can hard code that URL for now, and can deal with dynamic coding later)
 
@@ -12,7 +23,8 @@ async function main(): Promise<void> {
   // .then((res) => res.text()).then(parseCSV).catch(console.log)
 
   // For local testing, just read the sample file from the directory
-  const survey = await readFile("./surveys/CRT.csv")
+
+  const survey = await readFile(base_url + "/url=" + survey_url + "&" + ext)
     .then((buffer) => buffer.toString())
     .then(parseCSV)
     .catch(console.log);
