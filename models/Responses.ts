a3cef18db99aa0decc456fe46f672d7aa25d882d
Sumant R Shringari <61118035+sumants-dev@users.noreply.github.com>
2021-09-21 15:10:55 -0400

    updated responses schema

---
+++ models/Responses.ts
@@ -0,0 +1,23 @@
+import { Schema, model } from 'mongoose'
+
+type meta = 
+  | string
+  | string
+
+interface Responses {
+  survey_url: string
+  questions: [string]
+  answers: [[any]]
+  worker_id: [string | null]
+  metas: [meta]
+}
+
+const responses_schema = new Schema<Responses>({
+  survey_url: {type: String, required : true},
+  questions: [String], // Column Names of csv
+  answers: [[]],  // Rows of Csv [[answer1, answer2, ...], [answer1, answer2, ..]]
+  worker_id: [String], 
+  metas: [{ start_time: String, end_time: String }]
+})
+
+module.exports = model('response_schema', responses_schema)
