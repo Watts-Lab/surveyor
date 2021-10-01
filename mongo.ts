cfce071f315743d36bcf0c24b5e8acddefe2d118
Sumant R Shringari <61118035+sumants-dev@users.noreply.github.com>
2021-10-01 12:14:56 -0400

    Update mongo.ts

--- mongo.ts
+++ mongo.ts
@@ -7,27 +7,5 @@ var client = MongoClient.connect(
     process.env.PROD_URI,
 {
   tlsCAFile: `rds-combined-ca-bundle.pem` //Specify the DocDB; cert
-},
-function(err, client) {
-    if(err)
-        throw err;
-
-    //Specify the database to be used
-    const db = client.db('sample-database');
-
-    //Specify the collection to be used
-    const col = db.collection('sample-collection');
-
-    //Insert a single document
-    col.insertOne({'hello':'Amazon DocumentDB'}, function(err, result){
-      //Find the document that was previously written
-      col.findOne({'hello':'DocDB;'}, function(err, result){
-        //Print the result to the screen
-        console.log(result);
-
-        //Close the connection
-        client.close()
-      });
-   });
-});
+})
                     
