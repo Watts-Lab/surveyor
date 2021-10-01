2548eb9d5aead2c9b7b91da14c0f85b2ead640ba
Sumant R Shringari <61118035+sumants-dev@users.noreply.github.com>
2021-10-01 12:09:59 -0400

    Create mongo.ts

---
+++ mongo.ts
@@ -0,0 +1,34 @@
+var MongoClient = require('mongodb').MongoClient
+import { env } from "process";
+
+//Create a MongoDB client, open a connection to DocDB; as a replica set,
+//  and specify the read preference as secondary preferred
+
+var client = MongoClient.connect(
+    process.env.PROD_URI,
+{
+  tlsCAFile: `rds-combined-ca-bundle.pem` //Specify the DocDB; cert
+},
+function(err, client) {
+    if(err)
+        throw err;
+
+    //Specify the database to be used
+    const db = client.db('sample-database');
+
+    //Specify the collection to be used
+    const col = db.collection('sample-collection');
+
+    //Insert a single document
+    col.insertOne({'hello':'Amazon DocumentDB'}, function(err, result){
+      //Find the document that was previously written
+      col.findOne({'hello':'DocDB;'}, function(err, result){
+        //Print the result to the screen
+        console.log(result);
+
+        //Close the connection
+        client.close()
+      });
+   });
+});
+                    
