1b0886b4075e776ebc63eb88c3204734f752e1f7
Sumant R Shringari <61118035+sumants-dev@users.noreply.github.com>
2021-10-01 12:01:36 -0400

    Update prod_db.ts

--- databases/prod_db.ts
+++ databases/prod_db.ts
@@ -30,7 +30,6 @@ export default class Mongo implements Database_Wrapper {
   async test_database() {
     try {
       // Connect the client to the server
-      await this.client.connect();
       await this.client.db("admin").command({ ping: 1 });
       console.log("Connected successfully to mongodb server");
     } catch {
@@ -42,7 +41,6 @@ export default class Mongo implements Database_Wrapper {
   }
 
   async set_up() {
-    await this.client.connect()
     const database = this.client.db(this.db)
     const collection_obj = database.collection(this.collection)
     return collection_obj
@@ -51,29 +49,24 @@ export default class Mongo implements Database_Wrapper {
   async insert(json_body: any) {
     const collection_obj = await this.set_up()
     await collection_obj.insertOne(json_body)
-    await this.client.close()
     console.log('Inserted Document Sucessfully')
   }
 
   async delete(id: string) {
     const collection_obj = await this.set_up()
     await collection_obj.deleteOne({ _id: new ObjectID(id)})
-                        .then(() => this.client.close()) 
-    
     console.log('Document Deleted Sucessfully')
   }
 
   async find(query: any) {
     const collection_obj = await this.set_up()
     const queries = await collection_obj.find(query).toArray()
-    this.client.close()
     return queries
   }
 
   async update(filter: any, updateDoc: any, options: any) {
     const collection_obj = await this.set_up()
     const result = await collection_obj.updateOne(filter, updateDoc, options)
-    await this.client.close()
   }
 } 
 
