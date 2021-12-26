5656230a91d8195a828c32edd1fc3c4c8c2000af
Sumant R Shringari <61118035+sumants-dev@users.noreply.github.com>
2021-12-26 09:50:51 -0500

    Added New Packages
    Uninstalled Nedb, installed svg-capatcha

--- routes/encrypt.ts
+++ routes/encrypt.ts
@@ -7,14 +7,14 @@ import { verify_admin_token } from "../middlewares/auth.middleware"
 const router = express.Router()
 
 router.post("/encrypt", async (req, res) => { 
-    if (!req.body.url) { //only required parameter should be url
-        return res.status(400).send("Missing url field")
-    }
+  if (!req.body.url) { //only required parameter should be url
+    return res.status(400).send("Missing url field")
+  }
 
-    let text = JSON.stringify(req.body)
-    text = encrypt(text)
-    const return_url = env_config.DOMAIN + 'se/' + text
-    return res.send({'encrypted_url': return_url})
+  let text = JSON.stringify(req.body)
+  text = encrypt(text)
+  const return_url = env_config.DOMAIN + 'se/' + text
+  return res.send({'encrypted_url': return_url})
 })
 
 module.exports = router
