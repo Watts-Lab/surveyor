78d70c01833a16349eafb0b2794b094ec18e2603
Sumant R Shringari <61118035+sumants-dev@users.noreply.github.com>
2022-01-23 14:31:54 -0500

    remove node-fetch

--- app.ts
+++ app.ts
@@ -1,15 +1,9 @@
 /** @format */
 require("dotenv").config();
-import { parseCSV } from "./google_drive";
-// import fetch from "node-fetch";
 import { startServer } from "./server";
-import { readFile } from "fs/promises";
 
 async function main(): Promise<void> {
   // should be updated to Load CSV from URL (we can hard code that URL for now, and can deal with dynamic coding later)
-
-  // const survey = await fetch(process.env.survey_url)
-  // .then((res) => res.text()).then(parseCSV).catch(console.log)
   startServer();
 }
 
