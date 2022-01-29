1817d07439dbfe44bea8dfb1ecc301020eef61db
Sumant R Shringari <61118035+sumants-dev@users.noreply.github.com>
2022-01-29 11:26:59 -0500

    validations change flag
    We have more fine tune control of what validation go where and when

--- routes/links.ts
+++ routes/links.ts
@@ -55,41 +55,6 @@ router.post(`/link/survey`, verify_api_token, async (req, res) => {
 })
 
 
-
-/* THIS NEEDS TO BE AUTHENTICATED TO ADMIN USER
- Since this will be live.
- Current Hacky solution: Post enpoint root with random string 
- because someone not authenticate might
- stumble on to it when authentication is not set up.*/
-// Create New Link if it doesn't exist otherwise update
-
-//Note to self put this functionality in python backend services
-// will be deleted one I update the mturk hits pipeline not to depend on it
-router.post(`/link/${env_config.RANDOM}`, async (req, res) => {
-    const { alias, url } = req.body
-  
-    // Set old link if it exists to inactive
-    // With current logic there can only be one active link
-    await Db_Wrapper.update(
-      {alias, 'active': true}, {$set: {'active': false, 'end': Date().toString()}}, 
-      {}, 
-      'links'
-    )
-  
-    // create new alias if post request has been sent
-    // Only active link with this alias
-    await Db_Wrapper.insert({alias, url, 'hits': 0, 'active': true, 'start': Date().toString(), 'end': null}, 'links')
-    
-  
-    res.status(200).send('OK')
-  })
-
-  //Note to self put this functionality in python backend services
-  // will be deleted one I update the mturk hits pipeline
-router.get(`/link/${env_config.RANDOM}/:alias`, async (req, res) => {
-  const body = await Db_Wrapper.find({'alias': req.params.alias}, 'links')
-  res.status(200).send(body)
-})
 // Redirection To Mturk URL and increments count number
 router.get("/r/:alias", async (req, res) => {
   try { 
